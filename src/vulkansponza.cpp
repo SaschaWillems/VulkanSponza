d19b982f10baee56eee937cda64df1f23b355fe8
saschawillems <webmaster@saschawillems.de>
2016-05-09 21:47:47 +0200

    Initializer for mrt clear values

--- src/vulkansponza.cpp
+++ src/vulkansponza.cpp
@@ -934,7 +934,7 @@ public:
 		VkCommandBufferBeginInfo cmdBufInfo = vkTools::initializers::commandBufferBeginInfo();
 
 		// Clear values for all attachments written in the fragment sahder
-		std::array<VkClearValue, 4> clearValues;
+		std::array<VkClearValue, 4> clearValues = {};
 		clearValues[0].color = { { 0.0f, 0.0f, 0.0f, 0.0f } };
 		clearValues[1].color = { { 0.0f, 0.0f, 0.0f, 0.0f } };
 		clearValues[2].color = { { 0.0f, 0.0f, 0.0f, 0.0f } };
